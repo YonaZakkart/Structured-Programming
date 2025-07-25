29caffdc1a953dffeb499a2a6f3ac1a24d63e45b
YonaZakkart <musicyonathan.com>
2025-07-24 18:21:48 -0600

    Upgrading class 01

---
+++ 01Class/index.js
@@ -0,0 +1,26 @@
+let name = "Yonathan"
+let alias = "Yoni"
+let email = "yonatha@gmail.com"
+// console.log("Hola " + ${name} + "Como estas?")
+console.log(`Hola ${name}, como estas?`)
+
+//datos numericos
+let age = 25 //enntero
+let altura = 1.75 //decimal
+
+//datos bolea
+let isStudent = true //true o false
+let isTeacher = false //true o false
+
+//Tipo de dato underfined
+let notDefined; //no tiene valor asignado
+console.log(notDefined); //undefined
+
+//tipo de dato null
+let nullValue = null; //valor nulo
+
+//tipo de dato symbol
+let mySymbol = Symbol("unique"); //valor unico
+
+//tipo de dato bigint
+let myBigint = 12345678901234567890123456789890n; //numero entero grande
---
+++ 01Class/variables.js
@@ -0,0 +1,20 @@
+// var helloWorld = "Hello, World!";
+// var greeting = "Welcome to the JavaScript world!";
+// console.log(helloWorld);
+
+// variables let
+// console.log(greeting);
+// let helloWorld = "Hello, World!";
+// let greeting = "Welcome to the JavaScript world!";
+// console.log(helloWorld);
+
+// helloWorld = "Hello, Universe!";
+// console.log(helloWorld);
+
+//variable const
+const helloWorld = "Hello, World!";
+const greeting = "Welcome to the JavaScript world!";
+console.log(helloWorld);
+
+// helloWorld = "Hello, Universe!";
+// console.log(helloWorld);sssddada
